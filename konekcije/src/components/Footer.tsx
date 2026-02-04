e9d17264e3340005902c668abbfd5a4138ec0565
Faruk Kurtić <farukkurtic@gmail.com>
2026-02-04 01:35:57 +0100

    feat(konekcije): edit footer and add lucide icons

--- konekcije/src/components/Footer.tsx
+++ konekcije/src/components/Footer.tsx
@@ -1,11 +1,14 @@
+import { Github } from 'lucide-react';
 import './Footer.css';
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <div className="footer-content">
         <p>
-          Napravio{' '}
+          &copy; {currentYear} &nbsp;
           <a
             href="https://github.com/OpenSourceSarajevo"
             target="_blank"
@@ -14,15 +17,19 @@ export function Footer() {
             Open Source Sarajevo
           </a>
         </p>
-        <p className="footer-links">
+        
+        <div className="footer-links">
           <a
             href="https://github.com/OpenSourceSarajevo/igre"
             target="_blank"
             rel="noopener noreferrer"
+            className="github-link"
           >
+            <Github size={16} />
             GitHub
           </a>
-        </p>
+          
+        </div>
       </div>
     </footer>
   );
